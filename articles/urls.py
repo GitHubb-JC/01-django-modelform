4bea13bc65c9f5814c40ee012fea9d6a9cb65fbe
kdt-live <edu-live@hphk.kr>
2022-10-04 10:50:04 +0900

    07 - 게시글 생성하기(new, create)

--- articles/urls.py
+++ articles/urls.py
@@ -7,4 +7,8 @@ app_name = 'articles'
 urlpatterns = [
   # http://127.0.0.1:8000/articles/
   path('', views.index, name='index'),
+  # http://127.0.0.1:8000/articles/new/
+  path('new/', views.new, name='new'),
+  # http://127.0.0.1:8000/articles/create/
+  path('create/', views.create, name='create'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
 
 # Create your views here.
 
 # 요청 정보를 받아서..
 def index(request):
-
     # 페이지를 render...
     return render(request, 'articles/index.html')
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    # DB에 저장하는 로직
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    Article.objects.create(title=title, content=content)
+    return redirect('articles:index')
