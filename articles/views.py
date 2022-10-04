b7fa691669875d5f275aa1d1b7d9e2adc31670a2
kdt-live <edu-live@hphk.kr>
2022-10-04 10:57:41 +0900

    08 - 게시글 목록

--- articles/views.py
+++ articles/views.py
@@ -5,8 +5,13 @@ from .models import Article
 
 # 요청 정보를 받아서..
 def index(request):
-    # 페이지를 render...
-    return render(request, 'articles/index.html')
+    # 게시글을 가져와서..
+    articles = Article.objects.order_by('-pk')
+    # Template에 전달한다. 
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/index.html', context)
 
 def new(request):
     return render(request, 'articles/new.html')
