cfccc2dfdb92d9088acb8635d50fa736664179b0
kdt-live <edu-live@hphk.kr>
2022-10-12 11:24:16 +0900

    Login

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
 # from .models import User 
 from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import CustomUserCreationForm
 
@@ -27,7 +28,17 @@ def detail(request, pk):
     return render(request, 'accounts/detail.html', context)
 
 def login(request):
-    form = AuthenticationForm()
+    if request.method == 'POST':
+        # AuthenticationForm은 ModelForm이 아님!
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            # 세션에 저장
+            # login 함수는 request, user 객체를 인자로 받음 
+            # user 객체는 어디있어요? 바로 form에서 인증된 유저 정보를 받을 수 있음
+            auth_login(request, form.get_user())
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
     context = {
         'form': form
     }
