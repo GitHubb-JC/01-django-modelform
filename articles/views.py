94848163bc0e7d0f8e91997b40a873a08e114719
kdt-live <edu-live@hphk.kr>
2022-10-24 11:45:17 +0900

    Like - view/template

--- articles/views.py
+++ articles/views.py
@@ -93,3 +93,17 @@ def comment_create(request, pk):
         comment.user = request.user
         comment.save()
     return redirect('articles:detail', article.pk)
+
+@login_required
+def like(request, pk):
+    article = Article.objects.get(pk=pk)
+    # 만약에 로그인한 유저가 이 글을 좋아요를 눌렀다면,
+    # if article.like_users.filter(id=request.user.id).exists():
+    if request.user in article.like_users.all(): 
+        # 좋아요 삭제하고
+        article.like_users.remove(request.user)
+    else:
+        # 좋아요 추가하고 
+        article.like_users.add(request.user)
+    # 상세 페이지로 redirect
+    return redirect('articles:detail', pk)
