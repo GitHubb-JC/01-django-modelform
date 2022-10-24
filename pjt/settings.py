6b2aa38bfdef004b39dd146dfe7f8713105ee7a4
kdt-live <edu-live@hphk.kr>
2022-10-24 10:30:36 +0900

    Update style

--- pjt/settings.py
+++ pjt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#kkd731!xr*(cfl=42-3o-s8p9*b67$vqq19)rk$xj0dl21!#!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
